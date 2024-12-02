b8f0e322ecc37c1756a75e6f0abe3bf167d0712e
Misbahann <100833094+Misbahann@users.noreply.github.com>
2024-12-03 01:32:53 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,81 @@
+<?php
+include("koneksi.php");
+// query untuk menampilkan data
+$sql = 'SELECT * FROM data_barang';
+$result = mysqli_query($conn, $sql);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link href="style.css" rel="stylesheet" type="text/css" />
+    <title>Data Barang</title>
+</head>
+<style>
+    #table {
+  font-family: Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+#table td, #table th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+#table tr:nth-child(even){background-color: #f2f2f2;}
+
+#table tr:hover {background-color: #ddd;}
+
+#table th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #04AA6D;
+  color: white;
+}
+</style>
+<body>
+    <div class="container">
+        <h1>Data Barang</h1>
+        <a href="tambah.php">Tambah Barang</a>
+        <div class="main">
+            <table id="table">
+                <tr>
+                    <th>Gambar</th>
+                    <th>Nama Barang</th>
+                    <th>Katagori</th>
+                    <th>Harga Jual</th>
+                    <th>Harga Beli</th>
+                    <th>Stok</th>
+                    <th>id</th>
+                    <th>Aksi</th>
+                </tr>
+                <?php if ($result) : ?>
+                    <?php while ($row = mysqli_fetch_array($result)) : ?>
+                        <tr>
+                            <td><img src="gambar/<?= $row['gambar']; ?>" alt="<?=
+                                                                                $row['nama']; ?>"></td>
+                            <td><?= $row['nama']; ?></td>
+                            <td><?= $row['kategori']; ?></td>
+                            <td><?= $row['harga_beli']; ?></td>
+                            <td><?= $row['harga_jual']; ?></td>
+                            <td><?= $row['stok']; ?></td>
+                            <td><?= $row['id_barang']; ?></td>
+                            <td><a href="ubah.php?id=<?= $row['id_barang']; ?>">Ubah</a>
+                                <a href="hapus.php?id=<?= $row['id_barang']; ?>" onclick="return confirm('Apakah Anda yakin ingin menghapus data ini?')">Hapus</a>
+                            </td>
+                        </tr>
+                    <?php endwhile;
+                else : ?>
+                    <tr>
+                        <td colspan="7">Belum ada data</td>
+                    </tr>
+                <?php endif; ?>
+            </table>
+        </div>
+    </div>
+</body>
+
+</html>
